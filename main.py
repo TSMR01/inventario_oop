21b45808ad130692cda5bc57f5276675c1fd85cc
Héctor González <hector070123@gmail.com>
2026-07-15 12:20:09 -0600

    guardar cambios locales antes de combinar

--- main.py
+++ main.py
@@ -15,3 +15,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class Persona:
+    def __init__(self, nombre, edad, correo):
+        self.nombre = nombre
+        self.edad = edad
+        self.correo = correo
+
+    def mostrar_datos(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Edad: {self.edad}")
+        print(f"Correo: {self.correo}")
+
+
+def main():
+    persona = Persona(
+        "Héctor",
+        20,
+        "hector@correo.com"
+    )
+
+    persona.mostrar_datos()
+
+
+if __name__ == "__main__":
+    main()
